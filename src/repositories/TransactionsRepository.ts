575369918fd7d1268850a1c574299e34576c6950
Matheus Padilha <mvpadilha94@gmail.com>
2020-04-22 00:28:46 -0300

    fundamentos nodejs

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface AllTransactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,38 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): AllTransactions {
+    const allTransactions = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return allTransactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const findIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((total, { value }) => total + value, 0);
+
+    const findOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((total, { value }) => total + value, 0);
+
+    const balance = {
+      income: findIncome,
+      outcome: findOutcome,
+      total: findIncome - findOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
